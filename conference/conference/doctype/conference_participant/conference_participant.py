af6b0b8aa61b6d78650eb7b58c5ba155393576f1
Shivam Mishra <scm.mymail@gmail.com>
2019-11-07 14:14:55 +0530

    feat: added forms and other stuff

---
+++ conference/conference/doctype/conference_participant/conference_participant.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019, Frappe Technologies and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+
+class ConferenceParticipant(Document):
+	pass
+
+
+@frappe.whitelist(allow_guest=True)
+def register(name, email, organization, event="IndiaOS 2019"):
+	part = frappe.new_doc("Conference Participant")
+	part.full_name = name
+	part.email = email
+	part.organization = organization
+	part.event = event
+	part.save()
